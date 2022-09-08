c08faffab70988e6842df7a45a5ac8608f5b6e3e
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-09-08 08:51:33 -0300

    Initial Commit

---
+++ Trailer Tv/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trailer Tv
+//
+//  Created by kenjimaeda on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
